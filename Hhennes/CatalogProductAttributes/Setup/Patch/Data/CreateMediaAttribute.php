2f02a0d502185b8e5238d75a589dad1d9e636f71
Hervé Hennes <nenes_fr@yahoo.fr>
2020-10-29 10:17:58 +0100

    CatalogProductAttributes - create Media attribute ( image hover )

---
+++ Hhennes/CatalogProductAttributes/Setup/Patch/Data/CreateMediaAttribute.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Hhennes\CatalogProductAttributes\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+
+/**
+ * Class CreateMediaAttribute
+ * Create A Media Image Attribute
+ * @package Hhennes\CatalogProductAttributes\Setup\Patch\Data
+ */
+class CreateMediaAttribute implements DataPatchInterface
+{
+
+    /**
+     * @var EavSetupFactory
+     */
+    private EavSetupFactory $eavSetupFactory;
+
+    /**
+     * CreateMediaAttribute constructor.
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function apply()
+    {
+        $eavSetup = $this->eavSetupFactory->create();
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'hover_image',
+            [
+                'type' => 'varchar',
+                'label' => 'Hover Image',
+                'input' => 'media_image',
+                'required' => false,
+                'sort_order' => 30,
+                'frontend' => \Magento\Catalog\Model\Product\Attribute\Frontend\Image::class,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                'used_in_product_listing' => true,
+                'user_defined' => true,
+                'visible' => true,
+                'visible_on_front' => true
+            ]
+        );
+        $id = $eavSetup->getAttributeId(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'hover_image'
+        );
+
+        $attributeSetId = $eavSetup->getDefaultAttributeSetId(\Magento\Catalog\Model\Product::ENTITY);
+        $eavSetup->addAttributeToGroup(\Magento\Catalog\Model\Product::ENTITY, $attributeSetId, 'image-management', $id, 10);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
