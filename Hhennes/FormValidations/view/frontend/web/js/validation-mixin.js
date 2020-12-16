5f566d0df7af43af76920ca79c7c92900f0aa5c8
Hervé Hennes <nenes_fr@yahoo.fr>
2020-12-16 17:34:00 +0100

    Sample custom validations

--- Hhennes/FormValidations/view/frontend/web/js/validation-mixin.js
+++ Hhennes/FormValidations/view/frontend/web/js/validation-mixin.js
@@ -1,13 +1,19 @@
 define(['jquery'], function($) {
     'use strict';
     return function() {
-        console.log('add rule 5 words');
         $.validator.addMethod(
             'validate-five-words',
             function(value, element) {
                 return value.split(' ').length == 5;
             },
             $.mage.__('Please enter exactly five words')
-        )
+        );
+        $.validator.addMethod(
+            'validate-herve',
+            function(value, element) {
+                return value == 'herve';
+            },
+            $.mage.__('Please enter exactly herve')
+        );
     }
 });
