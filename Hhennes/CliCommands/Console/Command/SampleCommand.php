b8e55af36677b82fa3ca7a87f3f129599ffefa7a
Hervé Hennes <nenes_fr@yahoo.fr>
2021-01-25 13:41:17 +0100

    Add custom cli module + update README.md

---
+++ Hhennes/CliCommands/Console/Command/SampleCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Hhennes\CliCommands\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SampleCommand extends Command
+{
+
+    /**
+     * @inheritDoc
+     */
+    protected function configure()
+    {
+        $this->setName('hhennes:cli:samplecommand');
+        $this->setDescription('This is a sample basic command.');
+        parent::configure();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<info>Sample command without interactions</info>');
+    }
+
+}
--- README.md
+++ README.md
@@ -7,11 +7,18 @@ Samples modules for Magento 2.3 +
 > It also add a custom block in frontend to display the values of the attributes
 - CatalogProductAttributes
 > WIP
+- CliCommands
+> Add custom cli command  
 - CustomerAddressAttributes
 > WIP
 - CustomerAttributes
 > Programmatically create all types of customer attributes  
 > and display them in frontend and backend forms
+- CustomerExtensionAttributes
+> Programmatically create customer extension attributes  
+> and make them available in customer information
+- FormValidations
+> Display Js form validation examples and add customs  
 - OrderAttributes
 > Programmatically create attributes for order
 - QuoteAttributes
